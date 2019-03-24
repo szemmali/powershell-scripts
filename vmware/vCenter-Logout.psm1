667e7a57558de6855d7140b6805ea89ef6c1b2b3
Saddam ZEMMALI <saddam.zemmali@gmail.com>
2019-03-24 19:47:50 +0100

    Update vCenter-Logout.psm1

--- vmware/vCenter-Logout.psm1
+++ vmware/vCenter-Logout.psm1
@@ -1,3 +1,23 @@
+##========================================================================================
+##       Project:  Powershell scripts for VMware
+##        AUTHOR:  SADDAM ZEMMALI
+##         eMail:  saddam.zemmali@gmail.com
+##       CREATED:  01.01.2019 10:03:00
+##      REVISION:  --
+##       Version:  1.0  ¯\_(ツ)_/¯
+##    Repository:  
+##          Task:  Script IIS Reset for IIS CPU/RAM Issue on WWW/SECURE and IMAGE servers
+##          FILE:  Function-Reset-IIS.psm1
+##   Description:  Function vCenter-Login
+##   Requirement:  --
+##          Note:  -- 
+##       Example:  vCenter-Logout "*"       #  Logout from all vcenter 
+##                 vCenter-Logout "vcenter01"  
+##          BUGS:  Set-ExecutionPolicy -ExecutionPolicy Bypass
+##========================================================================================
+####################################
+#  Function to Logout from vCenter # 
+####################################
 function vCenter-Logout ($vcenter) {
   Write-Host "Disconnect to $vCenter..." -Foregroundcolor "Yellow" -NoNewLine 
   $disconnection =Disconnect-VIServer -Server $vCenter  -Force -confirm:$false  -ErrorAction SilentlyContinue -WarningAction 0 | Out-Null
@@ -11,5 +31,3 @@ function vCenter-Logout ($vcenter) {
   }
 }
 
-# Logout from all vcenter 
-vCenter-Logout "*"
