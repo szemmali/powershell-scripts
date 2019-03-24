92678bfa188d9d727cf154286999258a621c29fc
Saddam ZEMMALI <saddam.zemmali@gmail.com>
2019-03-24 19:46:08 +0100

    Update vCenter-Login.psm1

--- vmware/vCenter-Login.psm1
+++ vmware/vCenter-Login.psm1
@@ -1,4 +1,22 @@
-#vCenter-Login
+##========================================================================================
+##       Project:  Powershell scripts for VMware
+##        AUTHOR:  SADDAM ZEMMALI
+##         eMail:  saddam.zemmali@gmail.com
+##       CREATED:  01.01.2019 10:03:00
+##      REVISION:  --
+##       Version:  1.0  ¯\_(ツ)_/¯
+##    Repository:  
+##          Task:  Script IIS Reset for IIS CPU/RAM Issue on WWW/SECURE and IMAGE servers
+##          FILE:  Function-Reset-IIS.psm1
+##   Description:  Function vCenter-Login
+##   Requirement:  --
+##          Note:  -- 
+##       Example:  vCenter-Login 
+##          BUGS:  Set-ExecutionPolicy -ExecutionPolicy Bypass
+##========================================================================================
+#################################
+#  Function to Login in vCenter # 
+#################################
 function vCenter-Login {
   begin {
     $vcenter = Read-Host 'Enter The VCENTER FQDN'
@@ -10,11 +28,9 @@ function vCenter-Login {
     Write-Host "Connecting to $vCenter..." -Foregroundcolor "Yellow" -NoNewLine
     $connection = Connect-VIServer -Server $vCenter -Cred $vccredential -ErrorAction SilentlyContinue -WarningAction 0 | Out-Null
     If($? -Eq $True){
-      Write-Host "Connected" -Foregroundcolor "Green" 
-    }
+      Write-Host "Connected" -Foregroundcolor "Green" }
     Else{
-		  Write-Host "Error in Connecting to $vCenter; Try Again with correct username & password!" -Foregroundcolor "Red" 
-	  }
+      Write-Host "Error in Connecting to $vCenter; Try Again with correct username & password!" -Foregroundcolor "Red" }
   }
 }
 
